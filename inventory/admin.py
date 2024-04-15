76317e4b4e8c6d7239b04fa4ca74e23f510366bc
Anthony Lopez <Antl21292@gmail.com>
2024-04-15 17:34:54 -0400

    created models for django delights

--- inventory/admin.py
+++ inventory/admin.py
@@ -1,3 +1,8 @@
 from django.contrib import admin
+from .models import MenuItem, Ingredient, RecipeRequirement, Purchase
 
 # Register your models here.
+admin.site.register(MenuItem)
+admin.site.register(Ingredient)
+admin.site.register(RecipeRequirement)
+admin.site.register(Purchase)
--- inventory/models.py
+++ inventory/models.py
@@ -1,18 +1,54 @@
 from django.db import models
 
 
-# Create your models here.
-class Ingredients(models.Model):
-    pass
+class MenuItem(models.Model):
+    """
+    Represents an entry off the restaurant's menu
+    """
+    item_name = models.CharField(max_length=200, unique=True)
+    price = models.FloatField(default=0.00)
+
+    def __str__(self):
+        return f"title={self.title}; price={self.price}"
 
 
-class MenuItems(models.Model):
-    pass
+class Ingredient(models.Model):
+    """
+    Represents a single ingredient in the restaurant's inventory
+    """
+    name = models.CharField(max_length=200, unique=True)
+    quantity = models.FloatField(default=0)
+    unit = models.CharField(max_length=200)
+    price_per_unit = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"""
+        name={self.name};
+        qty={self.quantity};
+        unit={self.unit};
+        unit_price={self.price_per_unit}
+        """
 
 
 class RecipeRequirement(models.Model):
-    pass
+    """
+    Represents an ingredient required for a recipe for a MenuItem
+    """
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    ingredient = models.ForeignKey(Ingredient, on_delete=models.CASCADE)
+    quantity = models.FloatField(default=0)
+
+    def __str__(self):
+        return f"menu_item=[{self.menu_item.__str__()}]; ingredient={self.ingredient.name}; qty={self.quantity}"
 
 
-class Purchases(models.Model):
-    pass
+class Purchase(models.Model):
+    """
+    Represents a purchase of a MenuItem
+    """
+    menu_item = models.ForeignKey(MenuItem, on_delete=models.CASCADE)
+    date_purchased = models.DateTimeField(auto_now_add=True)
+
+    def __str__(self):
+        return f"menu_item=[{self.menu_item.__str__()}]; time={self.date_purchased}"
+
