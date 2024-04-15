5c6705611419b50b02b0dc1ee3af7999064aa996
Anthony Lopez <Antl21292@gmail.com>
2024-04-15 18:22:36 -0400

    routes added for inventory, menu, and purchases

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,7 +1,10 @@
 from django.urls import path
 
-from .views import HomePageView
+from .views import HomePageView, InventoryPageView, MenuItemsPageView, PurchasesPageView
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
+    path('inventory/', InventoryPageView.as_view(), name="inventory"),
+    path('menu/', MenuItemsPageView.as_view(), name="menu"),
+    path('purchases/', PurchasesPageView.as_view(), name="purchases"),
 ]
