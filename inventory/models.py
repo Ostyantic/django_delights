641d36f0e305198635b5d969f3fa2071edfb8fcd
Anthony Lopez <Antl21292@gmail.com>
2024-04-15 12:38:57 -0400

    small change

--- inventory/models.py
+++ inventory/models.py
@@ -1,3 +1,18 @@
 from django.db import models
 
+
 # Create your models here.
+class Ingredients(models.Model):
+    pass
+
+
+class MenuItems(models.Model):
+    pass
+
+
+class RecipeRequirement(models.Model):
+    pass
+
+
+class Purchases(models.Model):
+    pass
