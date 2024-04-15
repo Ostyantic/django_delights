cb4a62d70698e8170a6dd17d633f97a778690a19
Anthony Lopez <Antl21292@gmail.com>
2024-04-15 18:22:28 -0400

    views added for inventory, menu, and purchases

--- inventory/views.py
+++ inventory/views.py
@@ -1,6 +1,24 @@
 from django.shortcuts import render
-from django.views.generic import TemplateView
+from django.views.generic import TemplateView, ListView
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+
+from .models import Ingredient, MenuItem, RecipeRequirement, Purchase
 
 
 class HomePageView(TemplateView):
     template_name = 'home.html'
+
+
+class InventoryPageView(ListView):
+    template_name = 'pages/inventory.html'
+    model = Ingredient
+
+
+class MenuItemsPageView(ListView):
+    template_name = 'pages/menu.html'
+    model = RecipeRequirement
+
+
+class PurchasesPageView(ListView):
+    template_name = 'pages/purchases.html'
+    model = Purchase
