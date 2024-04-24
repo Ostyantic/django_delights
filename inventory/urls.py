1c6f02c7779b01524932560d78a066d5fc302c4a
Anthony Lopez <Antl21292@gmail.com>
2024-04-24 16:55:46 -0400

    work from 421

--- inventory/urls.py
+++ inventory/urls.py
@@ -1,10 +1,11 @@
 from django.urls import path
 
-from .views import HomePageView, InventoryPageView, PurchasesPageView, menu_items_page
+from .views import HomePageView, InventoryPageView, CreateInventoryView, PurchasesPageView, menu_items_page
 
 urlpatterns = [
     path('', HomePageView.as_view(), name="home"),
     path('inventory/', InventoryPageView.as_view(), name="inventory"),
+    path('inventory/add', CreateInventoryView.as_view(), name="add_inventory"),
     path('menu/', menu_items_page, name="menu"),
     path('purchases/', PurchasesPageView.as_view(), name="purchases"),
 ]
